79ce4d857571606991dda57ee318d5c0f60e26e1
HenrikDK2 <Henrik265@outlook.dk>
2020-07-02 02:30:26 +0200

    Loader

--- src/components/atoms/Loader.jsx
+++ src/components/atoms/Loader.jsx
@@ -1,28 +1,37 @@
 /** @jsx jsx */
 import React from "react";
 import { css, jsx, keyframes } from "@emotion/core";
-const load = keyframes`
-  0%{
-      opacity: 0.08;
-      filter: blur(5px);
-      letter-spacing: 3px;
-      }
+const load8 = keyframes`
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 `;
 
 const loaderStyle = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  margin: auto;
-  font-family: Helvetica, sans-serif, Arial;
-  animation: ${load} 1.2s infinite 0s ease-in-out;
-  animation-direction: alternate;
-  text-shadow: 0 0 1px white;
+  &,
+  &:after {
+    border-radius: 50%;
+    width: 10em;
+    height: 10em;
+  }
+  & {
+    margin: auto;
+    position: relative;
+    border-top: 1.1em solid #54ecdc;
+    border-right: 1.1em solid #54ecdc;
+    border-bottom: 1.1em solid #54ecdc;
+    border-left: 1.1em solid #ffffff;
+    animation: ${load8} 1.1s infinite linear;
+  }
 `;
 
 const Loader = ({}) => {
-  return <h2 css={loaderStyle}>Loading</h2>;
+  return <div css={loaderStyle}></div>;
 };
 
 export default Loader;
